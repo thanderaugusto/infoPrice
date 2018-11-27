21de3a2f5d7650a8fb2219313a47f018aa8ee74b
Thander Augusto <thander-99@hotmail.com>
2018-11-27 15:34:26 -0200

    91% finalizado

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req, res, next)=> {
     res.status(200).send({
         title: "Node Api InfoPrice",
-        version: "2.1.0"
+        version: "2.2.0"
     });
 });
 
